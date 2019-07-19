e9a19fcee66db0a092bdbef3bce9e174e07fef1f
ChristianBaduria <christian.baduria@gmail.com>
2019-07-19 07:21:30 -0400

    Added docstrings to the descriptor functions.

--- ConvertToDescriptor.py
+++ ConvertToDescriptor.py
@@ -12,6 +12,26 @@ face_rec_model = models["face rec"]
 shape_predictor = models["shape predict"]
 
 def image_to_descriptors(img_array, upscale=0):
+    """
+    Detects faces in an image array of shape=(H, W, 3) and turns
+    each one into a descriptor.
+    
+    Parameters
+    ----------
+    img_array : np.array[int, int(, 3)]
+        RGB image-array or 8-bit greyscale
+    
+    upscale : int, optional (default=0)
+        The number of times to upscale the image and reprocess it,
+        to find smaller faces
+    
+    Returns
+    -------
+    List[np.array[float]]
+        List of descriptor vectors of all detected faces in the
+        given img_array.
+        
+    """
     detections = list(face_detect(img_array, upscale))
     descriptors = []
     
@@ -23,7 +43,36 @@ def image_to_descriptors(img_array, upscale=0):
     return descriptors
 
 def camera_to_descriptors(upscale=0):
+    """
+    Detects faces from a photo taken by the camera.
+    
+    Parameters
+    ----------
+    upscale : *See image_to_descriptors() docstring*
+    
+    Returns
+    -------
+    List[np.array[float]]
+        *See image_to_descriptors() docstring*
+    
+    """
     return image_to_descriptors(take_picture(), upscale)
 
 def jpeg_to_descriptors(filepath, upscale=0):
+    """
+    Detects faces from a jpeg image file.
+    
+    Parameters
+    ----------
+    filepath : string
+        The file path to the jpeg image.
+    
+    upscale : *See image_to_descriptors() docstring*
+    
+    Returns
+    -------
+    List[np.array[float]]
+        *See image_to_descriptors() docstring*
+    
+    """
     return image_to_descriptors(io.imread(filepath), upscale)
