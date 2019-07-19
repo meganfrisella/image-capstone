63f4c880eaa3ccdade0b0c21081e47b43b87c913
Megan Frisella <mmfrisella@wpi.edu>
2019-07-19 10:16:38 -0400

    final database

--- database.py
+++ database.py
@@ -5,18 +5,16 @@ import pickle
 images = {"Megan": ["m1.jpeg", "m2.jpeg", "m3.jpeg"], "Vaishnavi": ["v1.JPG", "v2.JPG", "v3.JPG", "v4.JPG"],
           "Christian": ["c1.jpg", "c2.jpeg"]}
 myPath = "/Users/MeganFrisella/GitHub/Image-Capstone/images/"
-database = {}
 
 
 def generate_database():
+    database = {}
+
     for name in images:
-        print(name)
         descriptors = []
         for path in images[name]:
-            print(path)
-            descriptors = ConvertToDescriptor.jpeg_to_descriptors(myPath + path, upscale=0)
-
-        database[person] = person.Person(name, descriptors)
+            descriptors.append(ConvertToDescriptor.jpeg_to_descriptors(myPath + path, upscale=0))
+        database[name] = person.Person(name, descriptors)
 
     output = open('database.p', 'wb')
     pickle.dump(database, output)
@@ -29,6 +27,10 @@ def load_database(database_type):
     :param database_type: either "dict" to load as a dictionary or "list" to load as a list
     :return: the database of People in the form of a dictionary or list
     """
+    f = open("database.p", "rb")
+    database = pickle.load(f)
+    f.close()
+
     if database_type is "dict":
         return database
     elif database_type is "list":
