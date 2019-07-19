40cdfe0ace0e83c45eaf86fe601039403be798d3
VAddala9 <VAddala9@gmail.com>
2019-07-19 11:06:57 -0400

    fixed filepaths

--- WhispersTest.py
+++ WhispersTest.py
@@ -1,13 +1,13 @@
 import ConvertToDescriptor as convert
 import Whispers
 
-images = ["c1.jgp", "c2.jpeg", "m1.jpeg", "m2.jpeg", "v1.JPG", "v2.JPG"]
+images = ["images/c1.jgp", "images/c2.jpeg", "images/m1.jpeg", "images/m2.jpeg", "images/v1.JPG", "images/v2.JPG"]
 descriptors = list()
 file_paths = list()
 cutoff = 0.12
 
 for image in images:
-    ds = convert.jpeg_to_descriptors("c1.jpg")
+    ds = convert.jpeg_to_descriptors(image)
     descriptors.extend(ds)
     file_paths.extend([image for i in range(ds)])
 
